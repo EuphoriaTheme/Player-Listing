a36fbb884ab923128a747e3e56ab64ff4504b85b
Rep Graphics <charliebeckingham@hotmail.com>
2025-04-15 11:34:52 +0100

    More Tweaks SMH

--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-Route::get('/user/profile', [Pterodactyl\Http\Controllers\Admin\Extensions\{identifier}\{identifier}ExtensionController::class, 'fetchProfile'])->name('blueprint.extensions.playerlisting.wrapper.user.profile');
-
-Route::get('/admin/settings', [Pterodactyl\Http\Controllers\Admin\Extensions\{identifier}\{identifier}ExtensionController::class, 'index'])->name('blueprint.extensions.playerlisting.wrapper.admin.themeCustomiser');
-Route::post('/admin/save-settings', [Pterodactyl\Http\Controllers\Admin\Extensions\{identifier}\{identifier}ExtensionController::class, 'update'])->name('blueprint.extensions.playerlisting.wrapper.admin.themeCustomiser.save');
-?>
